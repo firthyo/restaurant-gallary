ec2f1be019ff84ae41ad4ad9620a0539e8e27c3b
firth <firth.maneesuksri@gmail.com>
2024-12-15 00:27:25 -0800

    added qr code

--- components/BookingTicket.tsx
+++ components/BookingTicket.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { format } from "date-fns";
 import { QrCode } from "lucide-react";
+import { QRCodeSVG } from "qrcode.react";
 
 interface BookingTicketProps {
   bookingData: {
@@ -20,16 +21,31 @@ const BookingTicket: React.FC<BookingTicketProps> = ({
   onDownload,
   onShare,
 }) => {
+  const [showQR, setShowQR] = useState(false);
+
+  const toggleView = () => setShowQR((prev) => !prev);
   return (
     <div className="max-w-md mx-auto overflow-hidden transition-all duration-300 ease-in-out transform hover:scale-105">
       <div className="bg-[#DBE4D6] p-6 rounded-[32px]">
         <div className="relative h-64 w-full mb-6">
-          <Image
-            src="https://images.unsplash.com/photo-1471253794676-0f039a6aae9d?q=80&w=2070&auto=format&fit=crop"
-            alt="Monet's Water Lilies"
-            className="rounded-2xl object-cover"
-            fill
-          />
+          {showQR ? (
+            <div className="flex items-center justify-center h-full w-full">
+              <QRCodeSVG
+                value={`https://example.com/reservation/${bookingData.confirmationCode}`}
+                size={170}
+                bgColor="#DBE4D6"
+                // fgColor="#2C3E50"
+                className="mx-auto"
+              />
+            </div>
+          ) : (
+            <Image
+              src="https://images.unsplash.com/photo-1471253794676-0f039a6aae9d?q=80&w=2070&auto=format&fit=crop"
+              alt="Reservation"
+              className="rounded-2xl object-cover"
+              fill
+            />
+          )}
           <div className="absolute top-4 right-4 flex gap-2 text-[#DBE4D6]">
             <span className="text-lg">05</span>
             <span className="text-lg">/</span>
@@ -83,10 +99,10 @@ const BookingTicket: React.FC<BookingTicketProps> = ({
               Share
             </Button>
             <Button
-              onClick={onDownload}
+              onClick={toggleView}
               className="flex-1 bg-[var(--card-foreground)] text-white "
             >
-              Download
+              See QR
             </Button>
           </div>
         </div>
